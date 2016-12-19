8032aa336478c8a7a84ee9a2fd5d9ef1ec2cad8f
Stella <stella02051994@gmail.com>
2016-12-19 01:10:59 -0800

    FEAT:finish update function

--- src/main/java/am/gitc/mportal/action/ProfileSettingsAction.java
+++ src/main/java/am/gitc/mportal/action/ProfileSettingsAction.java
@@ -1,6 +1,9 @@
 package am.gitc.mportal.action;
 
+import am.gitc.mportal.action.utils.MD5;
+import am.gitc.mportal.dao.impl.CountryDaoImpl;
 import am.gitc.mportal.dao.impl.UserDaoImpl;
+import am.gitc.mportal.domain.Country;
 import am.gitc.mportal.domain.User;
 import am.gitc.mportal.util.Global_Keys;
 import com.opensymphony.xwork2.ModelDriven;
@@ -13,10 +16,13 @@ public class ProfileSettingsAction extends GlobalAction implements ModelDriven<U
     private User user;
     private String newPassword;
     private UserDaoImpl userDao;
+    private int countryId;
+    private CountryDaoImpl countryDao;
 
 
     public ProfileSettingsAction() throws Exception {
-        userDao=new UserDaoImpl();
+        userDao = new UserDaoImpl();
+        countryDao = new CountryDaoImpl();
     }
 
 //    public String execute() throws Exception {
@@ -24,9 +30,16 @@ public class ProfileSettingsAction extends GlobalAction implements ModelDriven<U
 //        return SUCCESS;
 //    }
 
-    public String updateProfile() {
-        System.out.println(user);
-//        user.setPassword(newPassword);
+    public String updateProfile() throws Exception {
+        Country country = countryDao.getById(countryId);
+        int id = (Integer) mapSession.get(Global_Keys.LOGIN);
+        User users = userDao.getById(id);
+        users.setName(user.getName());
+        users.setSurname(user.getSurname());
+        users.setBirthDate(user.getBirthDate());
+        users.setPassword(MD5.encryptPassword(newPassword));
+        users.setCountry(country);
+        userDao.update(users);
         return SUCCESS;
     }
 
@@ -48,23 +61,31 @@ public class ProfileSettingsAction extends GlobalAction implements ModelDriven<U
         this.newPassword = newPassword;
     }
 
+    public int getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+
     @Override
-    public void validate(){
-      int id=(Integer) mapSession.get(Global_Keys.LOGIN);
+    public void validate() {
+        int id = (Integer) mapSession.get(Global_Keys.LOGIN);
         try {
-            if (!(user.getPassword().isEmpty()) && !(userDao.getById(id).getPassword()).equals(user.getPassword())) {
+            if (!(user.getPassword().isEmpty()) && !(userDao.getById(id).getPassword()).equals(MD5.encryptPassword(user.getPassword()))) {
                 if (newPassword.isEmpty()) {
                     addFieldError("user.password", "Please enter your right passsword");
-                }else if (newPassword.length()<2 || newPassword.length()>20){
-                    addFieldError("newPassword", "Please enter valid new");
+                } else if (newPassword.length() < 2 || newPassword.length() > 20) {
+                    addFieldError("newPassword", "Please enter valid new password");
+                } else if (!newPassword.equals(user.getConfirmPassword())) {
+                    addFieldError("user.confirmPassword", "Please enter valid confirmPassword");
                 }
-                else if(!newPassword.equals(user.getConfirmPassword())){
-                    addFieldError("user.confirmPassword","Please enter valid confirmPassword");
-                }
-            }
-            if(!newPassword.isEmpty()){
-                if(!newPassword.equals(user.getConfirmPassword())){
-                    addFieldError("user.confirmPassword","Please enter valid confirmPassword");
+            } else if (!newPassword.isEmpty()) {
+                if (user.getConfirmPassword().isEmpty()) {
+                    addFieldError("user.confirmPassword", "Please enter confirmPassword");
+                } else if (!newPassword.equals(user.getConfirmPassword())) {
+                    addFieldError("user.confirmPassword", "Please enter valid confirmPassword");
                 }
             }
         } catch (Exception e) {
